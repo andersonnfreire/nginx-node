87b6c72e9e97850419a47c0dfeb636d2b7f08c48
Anderson Freire <anderson_nascimento.16@hotmail.com>
2023-01-11 18:15:28 -0300

    feat: config nginx

--- node/server.js
+++ node/server.js
@@ -9,8 +9,9 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello Teste');
 });
+
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
