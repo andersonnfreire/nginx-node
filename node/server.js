37c61097acf53bcb9a26d47acc38d2a3de5650d9
Anderson Freire <anderson_nascimento.16@hotmail.com>
2023-01-12 00:35:28 -0300

    feat: rename in name people

--- node/server.js
+++ node/server.js
@@ -10,19 +10,22 @@ const database = require('./sqlConnection');
 app.get('/', (req, res) => {
   var sql = "CREATE TABLE IF NOT EXISTS people (id int not null auto_increment, name VARCHAR(255), primary key(id))";
   database.query(sql, (err, rows) => {
-    if(err) return res.status(500).send("Table Creation Failed");
+    if(err) {
+      return res.status(500).send("Table Creation Failed");
+    }
   });
 
   const sql_insert = "INSERT INTO people(name) VALUES ?";
   const values = [
-    ['teste1'],
-    ['teste1'],
-    ['teste3'],
-    ['teste4']
+    ['Anderson'],
+    ['Lucian'],
+    ['Wesley'],
+    ['Alex']
   ];
 
   database.query(sql_insert, [values], (err, rows) => {
       if (err) {
+        database.query();
         return res.status(500).send("Insertion Failed");
       }
   });
@@ -30,8 +33,10 @@ app.get('/', (req, res) => {
   const sql_select = "SELECT name FROM people";
   database.query(sql_select, function (error, result, field) {
     if (error) {
+      database.query();
       return res.status(500).send("Search Failed");
     } else {
+      database.query();
       res.send(`<h1>Full Cycle Rocks!</h1><br>` + Object.keys(result).map(function(k){return result[k].name}).join(","));
     }
   });
