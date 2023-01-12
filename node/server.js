35d59eaa643379bf146f2176932a58311e0e0eca
Anderson Freire <anderson_nascimento.16@hotmail.com>
2023-01-12 10:55:57 -0300

    feat: splitting code block into function

--- node/server.js
+++ node/server.js
@@ -7,31 +7,36 @@ const HOST = '0.0.0.0';
 
 const database = require('./sqlConnection');
 
-app.get('/', (req, res) => {
+function createTablePeople(res) {
   var sql = "CREATE TABLE IF NOT EXISTS people (id int not null auto_increment, name VARCHAR(255), primary key(id))";
-  database.query(sql, (err, rows) => {
+  return database.query(sql, (err, rows) => {
     if(err) {
+      database.query();
       return res.status(500).send("Table Creation Failed");
     }
   });
+}
+function insertPeople(res) {
 
-  const sql_insert = "INSERT INTO people(name) VALUES ?";
-  const values = [
+  var sql = "INSERT INTO people(name) VALUES ?";
+  var values = [
     ['Anderson'],
     ['Lucian'],
     ['Wesley'],
     ['Alex']
   ];
 
-  database.query(sql_insert, [values], (err, rows) => {
-      if (err) {
-        database.query();
-        return res.status(500).send("Insertion Failed");
-      }
+  return database.query(sql, [values], (err) => {
+    if (err) {
+      database.query();
+      return res.status(500).send("Insertion Failed");
+    }
   });
+}
 
-  const sql_select = "SELECT name FROM people";
-  database.query(sql_select, function (error, result, field) {
+function getPeople(res){
+  var sql = "SELECT name FROM people";
+  database.query(sql, function (error, result) {
     if (error) {
       database.query();
       return res.status(500).send("Search Failed");
@@ -40,6 +45,12 @@ app.get('/', (req, res) => {
       res.send(`<h1>Full Cycle Rocks!</h1><br>` + Object.keys(result).map(function(k){return result[k].name}).join(","));
     }
   });
+}
+
+app.get('/', (req, res) => {
+  createTablePeople(res);
+  insertPeople(res);
+  getPeople(res);
 });
 
 app.listen(PORT, HOST, () => {
